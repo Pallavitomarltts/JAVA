6e749c5806e83129bd6a98c654841bc3d196cca6
Pallavitomarltts <78848661+Pallavitomarltts@users.noreply.github.com>
2021-03-04 21:53:42 +0530

    Add files via upload

---
+++ Javaday_4/Teamans2.java
@@ -0,0 +1,26 @@
+
+public class Teamans2 {
+		String name;
+		long noofmatches;
+		
+		public Teamans2() {
+			super();
+		}
+		public Teamans2(String name, long noofmatches) {
+			super();
+			this.name = name;
+			this.noofmatches = noofmatches;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public long getNoofmatches() {
+			return noofmatches;
+		}
+		public void setNoofmatches(long noofmatches) {
+			this.noofmatches = noofmatches;
+		}
+	}
