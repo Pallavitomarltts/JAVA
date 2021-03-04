57ca0a11232008c58f2199048de0c25fc14f9936
Pallavitomarltts <78848661+Pallavitomarltts@users.noreply.github.com>
2021-03-04 23:03:04 +0530

    COMPARATOR
    COMPARATOR

---
+++ Activity-10/Playerans1.java
@@ -0,0 +1,29 @@
+
+	public class Playerans1 {
+		private String name;
+		private String skill;
+		public Playerans1(String name, String skill) {
+			super();
+			this.name = name;
+			this.skill = skill;
+		}
+		
+		public Playerans1() {
+			super();
+		}
+
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getSkill() {
+			return skill;
+		}
+		public void setSkill(String skill) {
+			this.skill = skill;
+		}
+		
+
+	}
